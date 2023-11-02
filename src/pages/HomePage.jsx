87b1ea35d08d00b47f041f609e431f03158bbb1b
Branson Smith <bransonsmith22@gmail.com>
2023-11-02 15:34:54 -0500

    Revise Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,26 +5,25 @@ export default function HomePage() {
 
     return <>
         
-        <div className="flex flex-row w-full my-5 mb-10">
-            <img src={bsdevlogo} alt='logo' className='m-auto mr-5 w-[120px] h-[120px] rounded-full border-2 border-gray-800'/>
-            <div className='flex flex-col m-8'>
+        <div className="flex flex-row w-full my-5 mb-10 flex-wrap">
+            <img src={bsdevlogo} alt='logo' className='m-auto my-5 w-[120px] h-[120px] rounded-full border-2 border-gray-800'/>
+            <div className='flex flex-col m-8 m-auto'>
                 <h1 className="m-auto ml-0 mb-0">Branson Smith</h1>
                 <h2 className="m-auto">Full Stack Software and Cloud Engineer</h2>
             </div>
         </div>
 
-        <p>
-            I have over 5 years of professional development experience, and have been programming for over a decade. 
+        <p className="text-justify text-sm">
+            I specialize in <b className="text-accent-300">Web and Cloud Technologies.</b> With over a decade of development experience, and being a certified AWS Solutions Architect, I can leverage my diverse skill set to build and improve all aspects of applications.
         </p>
-        <p>
-            I have mostly worked on Web Technologies hosted in AWS, and am a certified AWS Solutions Architect (Associate). I love solving problems and have put a variety of technologies in my toolbelt to help me do just that.
-        </p>
-        <p> 
-            AWS, Cloud, Serverless, C#, .Net, Python, REST APIs, Django, Azure Dev Ops Pipelines, RabbitMQ, Terraform, SQL, Microsoft SQL Server, PostgreSql, DynamoDb, Javascript, React, HTML, CSS
+        <p className="text-justify text-sm">
+            <b>Key skills:</b> AWS, Cloud, Serverless, C#, .NET, Python, REST APIs, Django, Azure DevOps, RabbitMQ, Terraform, SQL, MS SQL Server, PostgreSQL, DynamoDB, JavaScript, React, HTML, CSS, Google Cloud, GraphQL and more.
         </p>
 
-        <a href="/resume" className="py-2 px-4 mt-10">Resume</a>
-
-        <img src={bsdevhero} className='h-[200px] my-10 mx-auto opacity-40'/>
+        <div className="flex flex-row w-full flex-wrap">
+            <a href="/resume" className="py-3 px-4 mt-10 mx-auto rounded border-2 border-gray-800 w-fit h-fit">See my Resume</a>
+            <a href="/contact" className="py-3 px-4 mt-10 mx-auto rounded border-2 text-defaultText border-gray-800 w-fit h-fit">Contact Me</a>
+            <img src={bsdevhero} className='w-[75%] my-10 mx-auto opacity-40'/>
+        </div>
     </>
 }
