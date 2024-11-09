b273fc3555af86691db4f699b37a16ff39c05821
Branson Smith <bransonsmith22@gmail.com>
2024-11-09 13:10:41 -0600

    add auth0 config for amp dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <Auth0Provider
-    //     domain={import.meta.env.VITE_APP_AUTH0_DOMAIN}
-    //     clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID}
-    //     authorizationParams={{
-    //       redirect_uri: window.location.origin,
-    //       audience: import.meta.env.VITE_APP_AUTH0_AUD,
-    //       scope: "openid profile email"
-    //     }}
-    //   >
+    <Auth0Provider
+        domain={import.meta.env.VITE_APP_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: import.meta.env.VITE_APP_AUTH0_AUD,
+          scope: "openid profile email"
+        }}
+      >
       <App />
-    // </Auth0Provider>,
+    </Auth0Provider>,
 )
