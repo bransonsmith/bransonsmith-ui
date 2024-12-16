197aedce2be82edf1183831257439f16706dd1c1
Preston Cox <63611572+pcoxcode@users.noreply.github.com>
2024-12-15 20:00:18 -0600

    add reset function so that no accidental resets

--- src/GingerbreadCompetition/Pages/TeamSelector.jsx
+++ src/GingerbreadCompetition/Pages/TeamSelector.jsx
@@ -4,15 +4,19 @@ import TeamPage from "./TeamsPage";
 function TeamSelector() {
     const [teamCount, setTeamCount] = useState(0); // State to store selected number of teams
     const [selected, setSelected] = useState(null);
+    const [availableNumbers, setAvailableNumbers] = useState([4, 5, 6]);
 
   const handleTeamSelect = (e) => {
       const value = Number(e.target.value);
+      console.log(e)
       setTeamCount(value); // Update team count when a button is clicked
       setSelected(value);
+      setAvailableNumbers([value]);
   };
   const resetSelction = () => {
     setTeamCount(0);
-    setSelected(null);
+      setSelected(null);
+      setAvailableNumbers([4, 5, 6]);
   };
 
   const renderTeams = () => {
@@ -36,7 +40,7 @@ function TeamSelector() {
           <p></p>
       <h3>Teams of:</h3>
       <div className="flex-row bp-item">
-        {[4, 5, 6].map((num) => (
+        {availableNumbers.map((num) => (
           <button
             key={num}
             value={num}
